3133e25a6feccf8500064bd8d41657b155302066
ALEX\alex <lyssenko.alex@gmail.com>
2020-03-22 00:40:46 +0600

    feat: hanoi seson

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,12 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+  if (typeof date === 'undefined') return 'Unable to determine the time of year!';
+  if (Object.prototype.toString.call(date) !== "[object Date]") throw Error();
+  else {
+    let toReturn = "winter";
+    let month = date.getMonth() + 1;
+    if (month > 2 && month < 6 ) toReturn = "spring";
+    if (month > 5 && month < 9 ) toReturn = "summer";
+    if (month > 8 && month < 12 ) toReturn = "autumn";
+    return toReturn;
+  }
 };
