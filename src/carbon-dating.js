33fe53d790b5bcc25526dbf91f0b1c8d8e5d3c48
ALEX\alex <lyssenko.alex@gmail.com>
2020-03-22 00:25:12 +0600

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+
+module.exports = function dateSample( sampleActivity ) {
+
+  const MODERN_TIME = 15;
+  const HALF_LIFE= 5730;
+
+  let res = false;
+  if ( typeof sampleActivity === 'string' ) {
+      let activity = parseFloat(sampleActivity);
+      if ( activity > 0 && activity <= MODERN_TIME ) {
+        res = Math.ceil((Math.log(MODERN_TIME / activity)) / (0.693 / HALF_LIFE));
+        res = Math.abs(res);
+      }
+  }
+  return res;
 };
